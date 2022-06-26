83464ed176db864c8ba88701df5e39db62f2e661
darikko <61300092+darikko@users.noreply.github.com>
2022-06-26 14:13:53 -0500

    Add files via upload

---
+++ Food Truck.js
@@ -0,0 +1,17 @@
+function foodTruckFestival(menus) {
+    let flatMenus = menus.flat();
+
+    let combinedMenu = new Set();
+    flatMenus.forEach(item => {
+        combinedMenu.add(item);
+    });
+
+    const menuNode = document.querySelector("#combine-menu");
+    for (let item of combinedMenu) {
+        let foodNode = document.createElement('li');
+        foodNode.innerText = item;
+        menuNode.appendChild(foodNode);
+    }
+}
+
+foodTruckFestival([["Tacos", "Burgers"], ["Pizza"], ["Burgers", "Fries"]]);
